f320614515e0c45780dcf43076c008c8e7f67663
Leandro Foly <l.foly@gsuite.iff.edu.br>
2026-09-02 14:06:24 -0300

    Atualizado

--- consultas_firestore/lib/main.dart
+++ consultas_firestore/lib/main.dart
@@ -1,19 +1,24 @@
+import 'package:consultas_firestore/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'principal.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
 
 class App extends StatelessWidget {
-  const new({super.key});
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Exemplo Simples BD",
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.green),
-      home: Inclusao(),
+      title: 'Consultas Firestore',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Principal(),
     );
   }
 }
