928405039d6f0297562189f9c32b21bb1699b7e1
Leandro Foly <l.foly@gsuite.iff.edu.br>
2026-09-02 13:39:48 -0300

    atualização

--- consultas_firestore/lib/main.dart
+++ consultas_firestore/lib/main.dart
@@ -1,19 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'principal.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
 
 class App extends StatelessWidget {
-  const new({super.key});
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Exemplo Simples BD",
+      title: 'Controle de Cidades',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.green),
-      home: Inclusao(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Principal(),
     );
   }
 }
