8e9a8ba3a495c5dd84f565bd5a89393a0e1d72e9
Leandro Foly <l.foly@gsuite.iff.edu.br>
2026-09-16 14:22:05 -0300

    novo projeto exemplo

--- exemplo/test/widget_test.dart
+++ exemplo/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:agenda_firebase/main.dart';
+import 'package:exemplo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
